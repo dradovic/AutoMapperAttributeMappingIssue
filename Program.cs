17aebbe6405dd7fb8d9b125411c56d22c9b7b5c2
Dejan Appenzeller <dejan.appenzeller@gmail.com>
2019-06-18 11:06:33 +0200

    repro

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Reflection;
+
+namespace AttributeBasedMapping
+{
+    class Program
+    {
+        static void Main()
+        {
+            IServiceCollection services = new ServiceCollection();
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            var sp = services.BuildServiceProvider();
+            var mapper = sp.GetRequiredService<IMapper>();
+            var model = mapper.Map<ViewModel>(new Company { Id = 1, Name = "Straight Mapping" });
+            Console.WriteLine("ViewModel.Id: {0}", model.Id);
+            Console.WriteLine("ViewModel.Name: {0}", model.CompanyName);
+            var company = mapper.Map<Company>(new ViewModel { Id = 1, CompanyName = "Reverse Mapping" });
+            Console.WriteLine("Company.Id: {0}", company.Id);
+            Console.WriteLine("Company.Name: {0}", company.Name); // expected: 'Reverse Mapping' but is empty
+        }
+    }
+}
---
+++ ViewModel.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using AutoMapper.Configuration.Annotations;
+
+namespace AttributeBasedMapping
+{
+    [AutoMap(typeof(Company), ReverseMap = true)]
+    public class ViewModel
+    {
+        public int Id { get; set; }
+        [SourceMember(nameof(Company.Name))]
+        public string CompanyName { get; set; }
+    }
+}
